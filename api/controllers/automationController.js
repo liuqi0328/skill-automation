e675a47f301826f3511784b5384abb8025aede5d
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-05-08 14:18:14 -0400

    updated api controller

--- api/controllers/automationController.js
+++ api/controllers/automationController.js
@@ -11,6 +11,8 @@ const exec = require('child_process').execSync;
 const filepath = process.cwd() + '/temp';
 const shellScriptPath = process.cwd() + '/utils/create_package.sh';
 
+const credentialsErrMsg = {message: 'You need to provide credentials.'};
+
 exports.create_skill = (req, res) => {
   let data = req.body;
   let platform = data.platform;
@@ -22,8 +24,24 @@ exports.create_skill = (req, res) => {
     let err = {
       message: 'You need to choose a platform.',
     };
-    res.send(err);
+    return res.send(err);
+  } else {
+    console.log('platform: ', platform)
   }
+
+  let cred;
+  if (!data.credentials) {
+    return res.send(credentialsErrMsg);
+  } else {
+    console.log('credentials: ', data.credentials);
+    cred = data.credentials.split(',');
+
+    if (cred.length < 3) {
+      return res.send(credentialsErrMsg);
+    }
+  }
+
+  console.log('cred: ', cred);
 
   if (!fs.existsSync(filepath)) {
     fs.mkdirSync(filepath);
@@ -34,7 +52,14 @@ exports.create_skill = (req, res) => {
 
   switch (platform) {
     case 'alexa':
-      createAlexaSkillManifest(data, skillDirectory);
+      let awsAccessKey = cred[0];
+      let awsSecretKey = cred[1];
+      let awsRegion = cred[2];
+
+      console.log(awsAccessKey, awsSecretKey);
+
+      createAlexaSkillManifest(data, skillDirectory, underscoreName);
+      deployToAWSLambda();
       break;
     case 'google':
     case 'cortana':
@@ -46,7 +71,9 @@ exports.create_skill = (req, res) => {
   res.send(message);
 };
 
-function createAlexaSkillManifest(data, skillDirectory) {
+function createAlexaSkillManifest(data, skillDirectory, underscoreName) {
+  console.log('build started...');
+
   if (!fs.existsSync(`${skillDirectory}/models`)) {
     fs.mkdirSync(`${skillDirectory}/models`);
   }
@@ -54,26 +81,29 @@ function createAlexaSkillManifest(data, skillDirectory) {
     fs.mkdirSync(`${skillDirectory}/project`);
   }
 
-  let packageJSON = `{
-  "name": "2018_skill_automation",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \\"Error: no test specified\\" && exit 1",
-    "start": "nodemon server.js"
-  },
-  "author": "Fresh Digital Group",
-  "license": "ISC",
-  "devDependencies": {
-    "nodemon": "^1.17.4"
-  },
-  "dependencies": {
-    "express": "^4.16.3",
-    "mongoose": "^5.0.17"
-  }
-}`;
-  fs.writeFileSync(`${skillDirectory}/project/package.json`, packageJSON);
+  let packageJSON = {
+    name: underscoreName,
+    version: '1.0.0',
+    description: '',
+    main: 'index.js',
+    scripts: {
+      test: 'echo \"Error: no test specified\" && exit 1',
+      start: 'nodemon server.js'
+    },
+    author: 'Fresh Digital Group',
+    license: 'ISC',
+    devDependencies: {
+      nodemon: '^1.17.4'
+    },
+    dependencies: {
+      express: '^4.16.3',
+      mongoose: '^5.0.17'
+    }
+  };
+
+  console.log(packageJSON);
+
+  fs.writeFileSync(`${skillDirectory}/project/package.json`, JSON.stringify(packageJSON));
   console.log('package.json saved!');
 
   let text = `TEST1\nTEST2\nTEST3\n`;
@@ -82,11 +112,21 @@ function createAlexaSkillManifest(data, skillDirectory) {
 
   // fs.createReadStream(shellScriptPath).pipe(fs.createWriteStream(`${skillDirectory}/project/create_package.sh`));
 
+  if (!fs.existsSync(`${skillDirectory}/project/submission`)) {
+    exec(
+      `cd ${skillDirectory}/project && npm i && mkdir submission && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`,
+      { env: { ...process.env } },
+    );
+  } else {
+    exec(
+      `cd ${skillDirectory}/project && npm i && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`,
+      { env: { ...process.env } },
+    );
+  }
 
-  exec(
-    `cd ${skillDirectory}/project && npm i && mkdir submission && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`,
-    { env: { ...process.env } },
-  );
+  console.log('build finished...!');
+}
 
-  console.log('done...!');
+function deployToAWSLambda() {
+  console.log('deploying to AWS started...');
 }
