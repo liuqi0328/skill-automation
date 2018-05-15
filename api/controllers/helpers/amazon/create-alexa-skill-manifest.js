b230243b6e5ab7701ee80af62e8706fb667b4919
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-05-15 18:34:06 -0400

    updated alexa helpers

--- api/controllers/helpers/amazon/create-alexa-skill-manifest.js
+++ api/controllers/helpers/amazon/create-alexa-skill-manifest.js
@@ -6,9 +6,149 @@ const exec = require('child_process').execSync;
 module.exports = function(data, skillDirectory, underscoreName) {
   console.log('build started...');
 
+  // BUILD SKILL INTERACTION MODEL
   if (!fs.existsSync(`${skillDirectory}/models`)) {
     fs.mkdirSync(`${skillDirectory}/models`);
   }
+
+  let intents = [];
+  const skillName = underscoreName.replace(/_/g, ' ');
+  const languages = ['en-US', 'en-GB', 'en-AU', 'en-CA', 'en-IN'];
+  const interactionModel = {
+    'interactionModel': {
+      'languageModel': {
+        'invocationName': skillName.toLowerCase(),
+        'types': [],
+        'intents': intents,
+      },
+    },
+  };
+
+  languages.forEach(function(language) {
+    fs.writeFileSync(`${skillDirectory}/models/${language}.json`,
+                     JSON.stringify(interactionModel));
+  });
+
+  console.log('interaction model saved!');
+
+  // SKILL JSON
+  /**
+   * !!!!!!!!!!!!!!!!!!!!!UPDATE ICON URL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   */
+  const keyWord = [skillName];
+  const endPoint = `function:${underscoreName}`;
+  const testingInstructions = `TEST TESTING INSTRUCTIONS`;
+  const summary = `TEST SUMMARY`;
+  const description = `TEST DESCRIPTION`;
+  const examplePhrases = [
+    `Alexa, open ${skillName}.`,
+  ];
+  const category = 'test';
+  const smallIconSrc = `https://s3.amazonaws.com/${underscoreName}/assets/images/108/${underscoreName}108.png`;
+  const largeIconSrc = `https://s3.amazonaws.com/${underscoreName}/assets/images/512/${underscoreName}512.png`;
+  const privacyPolicyUrl = 'https://www.freshdigitalgroup.com/privacy-policy-for-bots';
+  const termsOfUseUrl = 'https://www.freshdigitalgroup.com/voice-applications-amazon-terms-of-use';
+
+  const skillJSON = {
+    'skillManifest': {
+      'publishingInformation': {
+        'locales': {
+          'en-US': {
+              'summary': summary,
+              'examplePhrases': examplePhrases,
+              'keywords': keyWord,
+              'smallIconUri': smallIconSrc,
+              'largeIconUri': largeIconSrc,
+              'name': skillName,
+              'description': description,
+          },
+          'en-GB': {
+              'summary': summary,
+              'examplePhrases': examplePhrases,
+              'keywords': keyWord,
+              'smallIconUri': smallIconSrc,
+              'largeIconUri': largeIconSrc,
+              'name': skillName,
+              'description': description,
+          },
+          'en-AU': {
+              'summary': summary,
+              'examplePhrases': examplePhrases,
+              'keywords': keyWord,
+              'smallIconUri': smallIconSrc,
+              'largeIconUri': largeIconSrc,
+              'name': skillName,
+              'description': description,
+          },
+          'en-CA': {
+              'summary': summary,
+              'examplePhrases': examplePhrases,
+              'keywords': keyWord,
+              'smallIconUri': smallIconSrc,
+              'largeIconUri': largeIconSrc,
+              'name': skillName,
+              'description': description,
+          },
+          'en-IN': {
+              'summary': summary,
+              'examplePhrases': examplePhrases,
+              'keywords': keyWord,
+              'smallIconUri': smallIconSrc,
+              'largeIconUri': largeIconSrc,
+              'name': skillName,
+              'description': description,
+          },
+        },
+        'isAvailableWorldwide': true,
+        'testingInstructions': testingInstructions,
+        'category': category,
+        'distributionCountries': [],
+      },
+      'apis': {
+        'custom': {
+          'endpoint': {
+            'uri': `arn:aws:lambda:us-east-1:429365556200:${endPoint}:LIVE`,
+          },
+        },
+      },
+      'manifestVersion': '1.0',
+      'privacyAndCompliance': {
+        'allowsPurchases': false,
+        'usesPersonalInfo': false,
+        'isChildDirected': false,
+        'isExportCompliant': true,
+        'containsAds': false,
+        'locales': {
+          'en-US': {
+            'privacyPolicyUrl': privacyPolicyUrl,
+            'termsOfUseUrl': termsOfUseUrl,
+          },
+          'en-GB': {
+            'privacyPolicyUrl': privacyPolicyUrl,
+            'termsOfUseUrl': termsOfUseUrl,
+          },
+          'en-AU': {
+            'privacyPolicyUrl': privacyPolicyUrl,
+            'termsOfUseUrl': termsOfUseUrl,
+          },
+          'en-CA': {
+            'privacyPolicyUrl': privacyPolicyUrl,
+            'termsOfUseUrl': termsOfUseUrl,
+          },
+          'en-IN': {
+            'privacyPolicyUrl': privacyPolicyUrl,
+            'termsOfUseUrl': termsOfUseUrl,
+          },
+        },
+      },
+    },
+  };
+  fs.writeFileSync(`${skillDirectory}/skill.json`,
+                   JSON.stringify(skillJSON));
+
+  console.log('skill.json saved!');
+
+  // BUILD SKILL LOGIC
   if (!fs.existsSync(`${skillDirectory}/project`)) {
     fs.mkdirSync(`${skillDirectory}/project`);
   }
@@ -33,26 +173,18 @@ module.exports = function(data, skillDirectory, underscoreName) {
       mongoose: '^5.0.17',
     },
   };
-  fs.writeFileSync(`${skillDirectory}/project/package.json`, JSON.stringify(packageJSON));
+  fs.writeFileSync(`${skillDirectory}/project/package.json`,
+                   JSON.stringify(packageJSON));
 
   console.log('package.json saved!');
 
-  let text = `TEST1\nTEST2\nTEST3\n`;
-  fs.writeFileSync(`${skillDirectory}/skill.json`, text);
-
-  console.log('skill.json saved!');
-
   // fs.createReadStream(shellScriptPath).pipe(fs.createWriteStream(`${skillDirectory}/project/create_package.sh`));
 
-  // CHANGE PATH FOR NPM
+  // NPM BUILD
   if (!fs.existsSync(`${skillDirectory}/project/submission`)) {
-    exec(
-      `cd ${skillDirectory}/project && npm i && mkdir submission && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`
-    );
+    exec(`cd ${skillDirectory}/project && npm i && mkdir submission && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`);
   } else {
-    exec(
-      `cd ${skillDirectory}/project && npm i && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`
-    );
+    exec(`cd ${skillDirectory}/project && npm i && zip -X -r submission/index.zip * -x build build/* *.xlsx Skills Skills/* test test/* speechAssets speechAssets/* index.zip deploy.sh > /dev/null`);
   }
 
   console.log('build finished...!');
